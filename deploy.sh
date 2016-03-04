1d6b64b6cf5d201b986a9464572b739c4a6a5831
msmyers <michael@topspectrum.com>
2016-03-03 20:24:58 -0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ trap 'echo "";echo "";echo "------------------------";echo "FAILED !";echo "----
 
 cd ../../topspectrum/topspectrum-common-webmvc; git pull; mvn clean install;
 cd ../../dotfeedback/dotfeedback-lib; git pull; mvn clean install;
-cd ../../dotfeedback/dotfeedback-free/src/main/javascript/; ember deploy production;
+cd ../../dotfeedback/dotfeedback-free/src/main/javascript/; rm -rf node_modules/; rm -rf bower_components/; npm install && bower install && ember deploy production;
 cd ../../../;
 cd ../../dotfeedback/dotfeedback-free; git pull; mvn clean package beanstalk:upload-source-bundle beanstalk:create-application-version beanstalk:update-environment

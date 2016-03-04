8074b6c385958ef522d06ba999cdb1b1c778f900
msmyers <michael@topspectrum.com>
2016-03-03 20:16:28 -0800

    removing

--- src/main/javascript/node_modules/ember-validations/addon/index.js
+++
@@ -1,6 +0,0 @@
-import Mixin from 'ember-validations/mixin';
-
-export default Mixin;
-export function validator(callback) {
-  return { callback: callback };
-}
--- src/main/javascript/node_modules/ember-validations/app/services/validations.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-var set = Ember.set;
-
-export default Ember.Service.extend({
-  init: function() {
-    set(this, 'cache', {});
-  }
-});
